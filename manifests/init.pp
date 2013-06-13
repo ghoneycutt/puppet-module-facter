2fd3afd5d632626f7924b826a331080d7dac2bb1
Ziad Meksassi <ziad.meksassi@ericsson.com>
2013-06-13 03:02:44 +0000

    Added Facter Package and Directory
    Facter package is ensured to be installed. Directory
    /etc/puppet/facter.d is ensured to be created and it's dependant on the
    Facter package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,17 @@
 # == Class: facter
 #
-# Full description of class facter here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { facter:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2013 Your name here, unless otherwise noted.
-#
+
 class facter {
 
-
+  package { 'facter':
+    ensure => installed,
+  }
+  
+  file { '/etc/puppet/facter.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['facter'],
+  }
 }
