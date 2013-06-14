1d5fe9355db0cabe809ad93c929b0bdead7343e3
Ziad Meksassi <ziad.meksassi@ericsson.com>
2013-06-14 13:22:05 -0400

    Fixed folder structure and added parameters
    The package and provider in the class are now parameters. The folder
    structure for facter has changed to be /etc/facter/facts.d

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,38 @@
 # == Class: facter
 #
+# Defines the facter class which ensures that the package facter is installed.
+# It also creates the dependant folders for facter /etc/facter/facts.d
 
 class facter {
 
-  package { 'facter':
-    ensure => installed,
-  }
+  package_name = 'facter',
+  package_provider = undef,
   
-  file { '/etc/puppet/facter.d':
+  dir_owner = 'root',
+  dir_group = 'root',
+  dir_mode = '0644',
+
+
+
+  package { 'facter':
+    ensure           => installed,
+    package_name     => $package_name,
+    package_provider => $package_provider,
+  }
+ 
+  file { '/etc/facter':
+    ensure => directory,
+    owner => $dir_owner,
+    group => $dir_group,
+    mode => $dir_mode,
+    require => Package['facter'],
+  }
+ 
+  file { '/etc/facter/facts.d':
     ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    owner => $dir_owner,
+    group => $dir_group,
+    mode => $dir_mode,
     require => Package['facter'],
   }
 }
