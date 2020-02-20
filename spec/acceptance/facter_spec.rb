225ac6cb40c9bf395d70194449a68db24c0ff508
Garrett Honeycutt <code@garretthoneycutt.com>
2020-02-19 23:24:17 -0500

    Add Windows acceptance tests and run with AppVeyor

--- spec/acceptance/facter_spec.rb
+++ spec/acceptance/facter_spec.rb
@@ -1,55 +1,192 @@
+
 require 'spec_helper_acceptance'
 
 describe 'facter class' do
+  if Gem.win_platform?
+    facts_d_dir = 'C:\ProgramData\PuppetLabs\facter\facts.d'
+    facts_d_owner = 'NT AUTHORITY\SYSTEM'
+    facts_d_group = 'NT AUTHORITY\SYSTEM'
+    facts_d_mode = nil
+    test_facts_file = 'C:\ProgramData\PuppetLabs\facter\facts.d\\test.txt'
+    facts_file = 'C:\ProgramData\PuppetLabs\facter\facts.d\\facts.txt'
+    facts_file_owner = 'NT AUTHORITY\SYSTEM'
+    facts_file_group = 'NT AUTHORITY\SYSTEM'
+    facts_file_mode = nil
+  else
+    facts_d_dir = '/etc/facter/facts.d'
+    facts_d_owner = 'root'
+    facts_d_group = 'root'
+    facts_d_mode = '755'
+    test_facts_file = '/etc/facter/facts.d/test.txt'
+    facts_file = '/etc/facter/facts.d/facts.txt'
+    facts_file_owner = 'root'
+    facts_file_group = 'root'
+    facts_file_mode = '644'
+  end
+
   context 'with default values for all parameters' do
     context 'it should be idempotent' do
-      it 'should work with no errors' do
-        pp = <<-EOS
-        include facter
-        EOS
+      pp = <<-EOS
+      include facter
+      EOS
 
-        # Run it twice and test for idempotency
-        apply_manifest(pp, :catch_failures => true)
-        apply_manifest(pp, :catch_changes  => true)
+      if Gem.win_platform?
+        manifest = 'C:\manifest-default.pp'
+
+        it 'creates manifest' do
+          File.open(manifest, 'w') { |f| f.write(pp) }
+          puts manifest
+          puts File.read(manifest)
+        end
+
+        describe command("PsExec -accepteula -s 'C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat' apply --debug #{manifest}") do
+          its(:stderr) { should contain('with error code 0') }
+        end
+
+        describe command("PsExec -accepteula -s 'C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat' apply --debug #{manifest}") do
+          its(:stderr) { should contain('with error code 0') }
+        end
+      else
+        it 'should work with no errors' do
+          # Run it twice and test for idempotency
+          apply_manifest(pp, :catch_failures => true)
+          apply_manifest(pp, :catch_changes  => true)
+        end
       end
     end
 
     context 'should contain resources' do
-      describe file('/etc/facter/facts.d') do
+      describe file(facts_d_dir) do
         it { should be_directory }
-        it { should be_owned_by 'root' }
-        it { should be_grouped_into 'root' }
-        it { should be_mode 0775 }
+        it { should be_owned_by facts_d_owner }
+        # Serverspec (more specifically
+        # Specinfra::Command::Windows::Base::File) does not support group, mode
+        # or size for files on Windows.
+        if host_inventory['platform'] != 'windows'
+          it { should be_grouped_into facts_d_group }
+          it { should be_mode facts_d_mode }
+        end
       end
 
-      describe file('/etc/facter/facts.d/facts.txt') do
+      describe file(facts_file) do
         it { should be_file }
-        it { should be_owned_by 'root' }
-        it { should be_grouped_into 'root' }
-        it { should be_mode 0644 }
-        its(:size) { should eq 0 }
+        it { should be_owned_by facts_file_owner }
+        # Serverspec (more specifically
+        # Specinfra::Command::Windows::Base::File) does not support group, mode
+        # or size for files on Windows.
+        if host_inventory['platform'] != 'windows'
+          it { should be_grouped_into facts_file_group }
+          it { should be_mode facts_file_mode }
+          its(:size) { should eq 0 }
+        end
       end
     end
   end
 
-  context 'with specifying facts' do
-    it 'should contain facts' do
+  context 'with specifying facts_hash' do
+    context 'should apply the manifest' do
       pp = <<-EOS
       class { 'facter':
         facts_hash => {
-          'test_fact' => 'test_value',
+          'test_fact' => {
+            value => 'test_value',
+            file  => 'test.txt',
+          },
         },
       }
       EOS
 
-      apply_manifest(pp, :catch_failures => true)
+      if Gem.win_platform?
+        manifest = 'C:\manifest-facts_hash.pp'
 
-      describe file('/etc/facter/facts.d/facts.txt') do
+        it 'creates manifest' do
+          File.open(manifest, 'w') { |f| f.write(pp) }
+          puts manifest
+          puts File.read(manifest)
+        end
+
+        describe command("PsExec -accepteula -s 'C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat' apply --debug #{manifest}") do
+          its(:stderr) { should contain('with error code 0') }
+        end
+      else
+        it 'should work with no errors' do
+          apply_manifest(pp, :catch_failures => true)
+        end
+      end
+    end
+
+    context 'and should contain facts' do
+      describe file(test_facts_file) do
         its(:content) { should match %r{test_fact=test_value} }
       end
 
       describe command('facter -p test_fact') do
         its(:stdout) { should contain('test_value') }
+      end
+    end
+  end
+
+  context 'with setting ensure_facter_symlink to true' do
+    if host_inventory['platform'] == 'windows'
+      before { skip('Windows does not have symlinks. Skipping test.') }
+    end
+
+    it 'should apply the manifest' do
+      pp = <<-EOS
+      class { 'facter':
+        ensure_facter_symlink => true,
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    context 'and should have the symlink' do
+      describe file('/usr/local/bin/facter') do
+        it { should be_symlink }
+        it { should be_linked_to '/opt/puppetlabs/bin/facter' }
+      end
+
+      describe command('/usr/local/bin/facter -p test_fact') do
+        its(:stdout) { should contain('test_value') }
+      end
+    end
+  end
+
+  context 'with specify purge_facts_d as true' do
+    context 'should apply the manifest' do
+      pp = <<-EOS
+      class { 'facter':
+        purge_facts_d => true,
+      }
+      EOS
+
+      if Gem.win_platform?
+        manifest = 'C:\manifest-purge_facts.pp'
+
+        it 'creates manifest' do
+          File.open(manifest, 'w') { |f| f.write(pp) }
+          puts manifest
+          puts File.read(manifest)
+        end
+
+        describe command("PsExec -accepteula -s 'C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat' apply --debug #{manifest}") do
+          its(:stderr) { should contain('with error code 0') }
+        end
+      else
+        it 'should work with no errors' do
+          apply_manifest(pp, :catch_failures => true)
+        end
+      end
+    end
+
+    context 'and should remove facts not managed by puppet' do
+      describe file(test_facts_file) do
+        it { should_not exist }
+      end
+
+      describe command('facter -p test_fact') do
+        its(:stdout) { should_not contain('test_value') }
       end
     end
   end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,11 +1,16 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/module_install_helper'
-require 'beaker/puppet_install_helper'
+if Gem.win_platform?
+  require 'serverspec'
+  set :backend, :cmd
+else
+  require 'beaker-rspec'
+  require 'beaker-puppet'
+  require 'beaker/module_install_helper'
+  require 'beaker/puppet_install_helper'
 
-run_puppet_install_helper
-install_module_dependencies
-install_module
+  run_puppet_install_helper
+  install_module_dependencies
+  install_module
+end
 
 RSpec.configure do |c|
   # Readable test descriptions
